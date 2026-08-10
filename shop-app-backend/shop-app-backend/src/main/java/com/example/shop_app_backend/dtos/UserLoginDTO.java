b012ffa4ba1e34582650c087c496bc6f57541346
phong2711 <145029451+phong2711@users.noreply.github.com>
2026-08-10 16:14:14 +0200

    feat: implement user controller and user DTOs

---
+++ shop-app-backend/shop-app-backend/src/main/java/com/example/shop_app_backend/dtos/UserLoginDTO.java
@@ -0,0 +1,19 @@
+package com.example.shop_app_backend.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginDTO {
+    @JsonProperty("phone_number")
+    @NotBlank(message = "Phone number is required")
+    private String phoneNumber;
+
+    @NotBlank(message = "Password cannot be blank")
+    private String password;
+}
